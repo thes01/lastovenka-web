f143c4241c9cf93bdceec6dde6f938751d12b625
Mira <athes01@gmail.com>
2025-04-08 20:05:15 +0200

    playing with slider

--- src/lib/PhotoSlider.svelte
+++ src/lib/PhotoSlider.svelte
@@ -1,19 +1,39 @@
 <script lang="ts">
 	import emblaCarouselSvelte from 'embla-carousel-svelte';
+	import type { EmblaCarouselType } from 'embla-carousel';
 
 	interface Props {
 		urls: string[];
 	}
 
+	let carousel: EmblaCarouselType;
+
 	const { urls }: Props = $props();
 </script>
 
-<div class="embla" use:emblaCarouselSvelte={{ options: { loop: true }, plugins: [] }}>
-	<div class="embla__container">
-		{#each urls as url}
-			<img src={url} alt="img" />
-		{/each}
+<div
+	class="embla"
+	use:emblaCarouselSvelte={{ options: { loop: true }, plugins: [] }}
+	onemblaInit={(event) => {
+		carousel = event.detail;
+		console.log(carousel);
+	}}
+>
+	<div class="embla__viewport">
+		<div class="embla__container">
+			{#each urls as url}
+				<img src={url} alt="img" />
+			{/each}
+		</div>
 	</div>
+	<button
+		class="embla__prev"
+		onclick={() => {
+			carousel.scrollPrev();
+			console.log('prev');
+		}}>Prev</button
+	>
+	<button class="embla__next" onclick={() => carousel.scrollNext()}>Next</button>
 </div>
 
 <style>
